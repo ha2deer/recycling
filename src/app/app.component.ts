81d66cee4daf6ff6636ac8ad32e0782ae7590cfe
ha2deer <hadermubarak.com>
2024-09-22 23:46:32 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
 import { NavComponent } from './nav/nav.component';
+  @Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, NavComponent, IntroComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+  })
+  export class AppComponent {
+    title = 'Recycling';
+  };
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,NavComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'Recycling';
-}
